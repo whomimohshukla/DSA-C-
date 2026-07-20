b0f08901191725215d331b57878652817ffca37f
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-07-21 04:22:34 +0530

    Refactor formatting and remove unnecessary blank lines in getSingleElement function

--- Array/Array-Basics/11countMaxium.cpp
+++ Array/Array-Basics/11countMaxium.cpp
@@ -7,23 +7,18 @@ public:
     int getSingleElement(vector<int>& arr) {
         int n = arr.size();
         int xorr = 0;
-
         // XOR all elements. Duplicates cancel out, leaving the single element.
         for (int i = 0; i < n; i++) {
             xorr = xorr ^ arr[i];
         }
-
         return xorr;
     }
 };
 
 int main() {
     vector<int> arr = {4, 1, 2, 1, 2};
-
     Solution obj;
     int ans = obj.getSingleElement(arr);
-
     cout << "The single element is: " << ans << endl;
-
     return 0;
 }
